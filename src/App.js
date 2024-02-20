4ae9c079c65ec4be036452b653161a3c13b17ca3
planitary <853652156@qq.com>
2024-02-20 22:37:09 +0800

    20240220 添加删除，排序，tab切换功能

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.scss'
 import avatar from './images/bozai.png'
 import avatar2 from './images/IMG_3445.JPG'
 import avatar3 from './images/__original_drawn_by_ama_mitsuki__5af6796539d372d39894afcc25707fa7.png'
+import avatar4 from './images/o_3d8cd2c44511cb1eafab60ebc1f36dab.jpg'
 import {useState} from "react";
+import _ from 'lodash'
+import classNames from "classnames";
 
 /**
  * 评论列表的渲染和操作
  *
  * 1. 根据状态渲染评论列表
- * 2. 删除评论
+ * 2. 删除评论(仅能删除自己的)
  */
 
 // 评论列表数据
@@ -26,7 +29,7 @@ const defaultList = [
         content: '哎哟，不错哦',
         // 评论时间
         ctime: '10-18 08:15',
-        like: 88,
+        like: 173,
     },
     {
         rpid: 2,
@@ -37,7 +40,7 @@ const defaultList = [
         },
         content: '我寻你千百度 日出到迟暮',
         ctime: '11-13 11:29',
-        like: 88,
+        like: 4,
     },
     {
         rpid: 1,
@@ -48,8 +51,19 @@ const defaultList = [
         },
         content: '学前端就来黑马',
         ctime: '10-19 09:00',
-        like: 66,
+        like: 12,
     },
+    {
+        rpid: 4,
+        user: {
+            uid: '83294322',
+            avatar: avatar4,
+            uname: '爱脱鞋的女子',
+        },
+        content: '不错不错啊，建议快点更新',
+        ctime: '04-02 15:22',
+        like: 700
+    }
 ]
 // 当前登录用户信息
 const user = {
@@ -60,6 +74,7 @@ const user = {
     // 用户昵称
     uname: '黑马前端',
 }
+
 
 /**
  * 导航 Tab 的渲染和操作
@@ -79,8 +94,29 @@ const tabs = [
 
 const App = () => {
     // 渲染评论列表
-    // 1、使用useState维护列表
-    const [commentList, setCommentList] = useState(defaultList)
+    // 1、使用useState维护列表(初始按照最热排序，因为106行指定默认tab为最热)
+    const [commentList, setCommentList] = useState(_.orderBy(defaultList,'like','desc'))
+    // 删除评论
+    const handleDel = (id) => {
+        console.log(id)
+        setCommentList(commentList.filter(item => item.rpid !== id))
+
+    }
+    // tab切换功能
+    // 记录点击的type，遍历整个数组，记录的type命中时则高亮
+    const [type,setType] = useState('hot')
+    const handleChangeType = (type) => {
+        console.log(type)
+        setType(type)
+        // 基于列表的排序，根据type类型排序，如果是最新(time)，按照时间；如果是最热(hot)，按照点赞数
+        // 引入lodash（封装的js库)
+        if (type === 'hot'){
+            setCommentList(_.orderBy(commentList,'like','desc'))
+        }
+        if (type === 'time'){
+            setCommentList(_.orderBy(commentList,'ctime','desc'))
+        }
+    }
 
     return (
         <div className="app">
@@ -90,12 +126,21 @@ const App = () => {
                     <li className="nav-title">
                         <span className="nav-title-text">评论</span>
                         {/* 评论数量 */}
-                        <span className="total-reply">{10}</span>
+                        <span className="total-reply">{4}</span>
                     </li>
                     <li className="nav-sort">
                         {/* 高亮类名： active */}
-                        <span className='nav-item'>最新</span>
-                        <span className='nav-item'>最热</span>
+                        {/*模板字符串，动态生成css样式*/}
+
+                        {tabs.map(item => <span
+                            key={item.type}
+
+                            // className={`nav-item ${type === item.type && 'active'}`}
+                            // 使用classNames简化 classNames(param,key:value)
+                            // 参数——静态类名:要修改的类名，key:要修改的css类名 value:条件，为true时执行
+                            className={classNames('nav-item',{active: type === item.type})}
+                            onClick={() => handleChangeType(item.type)}>{item.text}</span>
+                        )}
                     </li>
                 </ul>
             </div>
@@ -113,7 +158,7 @@ const App = () => {
                         {/* 评论框 */}
                         <textarea
                             className="reply-box-textarea"
-                            placeholder="发一条友善的评论"
+                            placeholder="说点什么吧~~"
                         />
                         {/* 发布按钮 */}
                         <div className="reply-box-send">
@@ -124,7 +169,7 @@ const App = () => {
                 {/* 评论列表 */}
                 <div className="reply-list">
                     {/* 评论项 */}
-                  {commentList.map(item => (
+                  {commentList.map(item =>
                       <div key={item.rpid} className="reply-item">
                         {/* 头像 */}
                         <div className="root-reply-avatar">
@@ -150,15 +195,18 @@ const App = () => {
                               <span className="reply-time">{item.ctime}</span>
                               {/* 评论数量 */}
                               <span className="reply-time">点赞数:{item.like}</span>
-                              <span className="delete-btn">
-                    删除
-                  </span>
+                                {/* 删除显示-条件渲染，拿到当前项id，以此id为条件对评论列表做过滤or直接讲将内容清空*/}
+                                {user.uid === item.user.uid && <span className="delete-btn"
+                                                                     onClick={() => handleDel(item.rpid)}>
+                                    删除
+                                </span>}
+
 
                             </div>
                           </div>
                         </div>
                       </div>
-                  ))}
+                  )}
                 </div>
             </div>
         </div>
