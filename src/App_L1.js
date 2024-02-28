340fcca18011f39c2278e44bc99f8a7e1be46e44
planitary <853652156@qq.com>
2024-02-28 22:45:42 +0800

    20240228 add:learn1&learn2主文件

---
+++ src/App_L1.js
@@ -0,0 +1,121 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// 项目根组件
+// const count = 100;
+// function getName(){
+//     return 'jack';
+// }
+// 列表渲染
+import {useState} from "react";
+import './index.css'
+
+const list = [
+    {id: 1001, name: 'Vue'},
+    {id: 1002, name: 'React'},
+    {id: 1003, name: 'Angular'}
+]
+const isLogin = false
+
+const articleType = 3
+
+
+
+function getArticleTemplate(){
+    if (articleType === 0){
+        return <div>我是无图文章</div>
+    }else if(articleType === 1){
+        return <div>我是单图文章</div>
+    }
+    else if (articleType === 3){
+        return <div>我是三图文章</div>
+    }
+}
+
+// 定义组件(首字母大写)
+function Button(){
+    // 业务逻辑
+    return <button>click me !</button>
+}
+
+function buttonClick2(){
+    console.log("按钮被点击了2")
+}
+
+
+
+function App() {
+    // 数据驱动绑定（useState实现计数器)
+    // useState返回的是一个数组，count表示状态状态变量，setCount表示修改状态变量的方法
+    // 1、调用useState添加一个状态变量,构造函数的值表示计数的初值
+    const [count,setCount] = useState(0)
+    // 2、点击事件回调函数,每次点击时，count+1(setCount会更新新的count值)
+    const handleClick = () => {
+        setCount(count + 1)
+    }
+    // 再来一个exp
+    const [form,setForm] = useState({name:'jack'})
+    const handleChangeName = () => {
+        // ...为展开运算符，方便地将可迭代对象的元素或属性展开，以便更容易地组合、复制或传递参数
+        setForm({
+            ...form,name: 'peter'
+        })
+    }
+
+    // 回调函数并设置形参
+    const buttonClick = (e) => {console.log("按钮被点击了",e)}
+    // 箭头函数设置入参
+    const buttonClick3 = (name) => {console.log("欢迎",name)}
+    // 自定义入参+事件参数
+    const buttonClick4 = (name,e) => {console.log("欢迎~",name,e)}
+
+
+    return (
+        <div className="App">
+            this is APP
+            {/*{"this is message"}*/}
+            {/*{count}*/}
+            {/*my name is {getName()}*/}
+            {/*today is {new Date().getDate()}*/}
+            {/*<div style={{color: 'red'}}> this is div</div>*/}
+
+            {/*    渲染列表,使用匿名函数，=>右侧为返回值，需要哪个字段就用哪个字段进行返回，在进行行的渲染是
+            指定一个key可以增加渲染速度*/}
+            <ul>
+                {list.map(item => (<li key={item.id}>{item.name}</li>))}
+            </ul>
+        {/*    逻辑与*/}
+            {isLogin && <span>this is span when true</span>}
+            <br/>
+        {/*    三元运算符*/}
+            {isLogin ? <span>jack</span> : <span style={{color: 'red'}}>no user!</span>}
+
+            <br/>
+            {getArticleTemplate()}
+
+        {/*    事件绑定*/}
+            <button onClick={buttonClick}>click me</button>
+            <button onClick={() => buttonClick3("planitary")}>登录</button>
+        {/*    自定义入参+事件参数*/}
+            <button onClick={(e) => buttonClick4("peter",e)}>登录+事件</button>
+            <br/>
+            <p>组件</p>
+        {/*    自闭和使用组件*/}
+            <Button/>
+        {/*    成对标签使用组件*/}
+            <Button>成对</Button>
+            <br/>
+            <p>一下为数据驱动计数器</p>
+            <button onClick={handleClick}>{count}</button>
+            <button onClick={handleChangeName}>修改表单name:{form.name}</button>
+            <br/>
+            <p>通过css来设置组件样式</p>
+            <span className='foo'>this is span</span>
+
+        </div>
+
+
+    );
+}
+
+export default App;
---
+++ src/App_L2.js
@@ -0,0 +1,157 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// 受控绑定表单
+// 1、声明一个react状态
+
+// 2、核心绑定流程(a.通过value属性绑定react状态，b.绑定事件，通过时间参数e拿到最新值，反向修改到react状态
+
+// react获取dom 1、useRef生成对象，绑定到dom对象上 2、dom可用是，useRef获取dam(dom渲染完毕后)
+
+// 组件通信-父传子(1、父组件传递数据，子组件标签绑定属性 2、子组件使用props接受数据
+
+import {createContext, useContext, useEffect, useRef, useState} from "react";
+
+// 下方的APP是父组件，这里额外定义一个子组件
+function Son(props){
+    // 这里的props是个对象，包含了父组件传递过来的所有数据；他是个只读对象，不能直接修改，要修改只能依赖父组件
+    console.log(props)
+    return <div>this is son & his father is {props.name},it has jsx:{props.jsx}
+
+    </div>
+}
+// 子组件的函数
+function Son2({onGetSonMsg}){
+    const sonMsg = 'this is Son!'
+    return <button onClick={() => onGetSonMsg(sonMsg)}>sendMsg</button>
+}
+
+// 组件跨层通信，APP-A-B，即APP将数据传递给B
+/** 组件跨层通信，APP-A-B，即APP将数据传递给B
+ * 1、 通过createContext方法创建一个上下文对象
+ * 2、在顶层组件 通过provider组件提供数据
+ * 3、在底层组件 通过useContext钩子函数使用数据
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+function A(){
+    return (
+        <div>this is A component
+        <B/>
+        </div>
+    )
+}
+
+function B(){
+    const topMsgByApp = useContext(msgCtx)
+    return (
+        <div>this is B component,{topMsgByApp}</div>
+    )
+}
+
+// 上下文对象，组件跨层通信时用到
+const msgCtx = createContext('');
+// 页面渲染完毕后，请求列表（服务端）
+const url = 'http://geek.itheima.net/v1_0/channels'
+const url2 = 'http://localhost:8080/interface/interfaceList'
+
+function App() {
+    // 父组件属性
+    const fatherName = 'this is App name'
+    // 子传父，子组件将函数返回给父组件，父组件进行调用
+    const [msg,setMsg] = useState('');
+
+    const getSonMsg = (msg) => {
+        console.log(msg)
+        setMsg(msg);
+    }
+    const [value,setValue] = useState('')
+    const inputRef = useRef(null)
+    const showDom = () => {
+        console.log(inputRef.current)
+    }
+
+    // 拿到数据，完成渲染
+    const [list,setList] = useState([])
+    // 页面加载结束后的请求，用于渲染数据：useEffect
+    useEffect(() => {
+        // 额外操作，获取列表
+        async function getList(){
+            const res = await fetch(url);
+            // json处理
+            const jsonRes = await res.json()
+            console.log(list)
+            setList(jsonRes.data.channels);
+        }
+        getList()
+    },[])
+
+    const[interfaceList,setInterfaceList] = useState([])
+    useEffect(() => {
+        async function getInterfaceList(){
+            const reqBody = {
+                projectId:'42572254526',
+            };
+
+            const res = await fetch(url2,{
+                method: 'POST',
+                headers:{'Content-Type':'application/json'},
+                body: JSON.stringify(reqBody),
+            });
+            const jsonRes = await res.json()
+            console.log(interfaceList)
+            setInterfaceList(jsonRes.items)
+        }
+        getInterfaceList()
+        // 这里deps的参数表示渲染的时机，当传入空数组，表示只在渲染完毕后执行一次;当不传时表示只在组件初始化&更新时执行;
+        // 当传入特定依赖项时（通常是某个函数）表示只在组件初始化&满足特定依懒项时执行
+    },[])
+
+    return (
+        <div>
+            <p>将表单状态与react进行绑定，react状态改变，表单内容随之改变</p>
+            <input value={value} onChange={(e) => setValue(e.target.value)}
+            type='text'/>
+            {/*<br/>*/}
+            <p>react获取dom</p>
+            <input type='text' ref={inputRef}/>
+            {/*按钮模拟渲染dom*/}
+            <button onClick={showDom}>获取dom</button>
+            {/*父组件传递时，可以传递任何东西*/}
+            <Son name = {fatherName}
+            age = {18}
+            flag = {true}
+            list=  {[1,3,5,7,9]}
+            obj = {{id:'223',orderNo: '374279482397'}}
+            func = {() => console.log(123)}
+            jsx = {<span color="red">this is span</span>}/>
+            <p>字传父</p>
+            <span>{msg}</span>
+            <Son2 onGetSonMsg={getSonMsg}/>
+            <p>跨层通信</p>
+        {/*    跨层通信*/}
+            <msgCtx.Provider value={fatherName}>
+                topMsg!
+                <A/>
+            </msgCtx.Provider>
+            <p>
+                useEffect-页面加载结束后的请求，用于渲染数据
+            </p>
+            <ul>
+                {list.map(item => <li key={item.id}>{item.name}</li>)}
+            </ul>
+
+            <p>
+                测试接口
+            </p>
+            <ul>
+                {interfaceList.map(item => <li key={item.id}>{item.interfaceName}</li>)}
+            </ul>
+
+
+        </div>
+    );
+}
+
+export default App;
