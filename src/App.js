c52122ec3f88c532c1cefb8b9b759d848fc3067e
planitary <853652156@qq.com>
2024-03-05 22:10:56 +0800

    20240305 mod:组件封装

--- src/App.js
+++ src/App.js
@@ -95,6 +95,46 @@ const tabs = [
     {type: 'time', text: '最新'},
 ]
 
+// 封装item组件
+function Item({item,onDel}){
+    return (<div key={item.rpid} className="reply-item">
+            {/* 头像 */}
+            <div className="root-reply-avatar">
+                <div className="bili-avatar">
+                    <img
+                        className="bili-avatar-img"
+                        alt=""
+                        src={item.user.avatar}
+                    />
+                </div>
+            </div>
+
+            <div className="content-wrap">
+                {/* 用户名 */}
+                <div className="user-info">
+                    <div className="user-name">{item.user.uname}</div>
+                </div>
+                {/* 评论内容 */}
+                <div className="root-reply">
+                    <span className="reply-content">{item.content}</span>
+                    <div className="reply-info">
+                        {/* 评论时间 */}
+                        <span className="reply-time">{item.ctime}</span>
+                        {/* 评论数量 */}
+                        <span className="reply-time">点赞数:{item.like}</span>
+                        {/* 删除显示-条件渲染，拿到当前项id，以此id为条件对评论列表做过滤or直接讲将内容清空*/}
+                        {user.uid === item.user.uid && <span className="delete-btn" onClick={()=> onDel(item.rpid)}
+                                                            >
+                                    删除
+                                </span>}
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 // 自定义封装hook函数
 function useGetList(){
     const [commentList, setCommentList] = useState([])
@@ -221,43 +261,7 @@ const App = () => {
                 {/* 评论列表 */}
                 <div className="reply-list">
                     {/* 评论项 */}
-                    {commentList.map(item =>
-                        <div key={item.rpid} className="reply-item">
-                            {/* 头像 */}
-                            <div className="root-reply-avatar">
-                                <div className="bili-avatar">
-                                    <img
-                                        className="bili-avatar-img"
-                                        alt=""
-                                        src={item.user.avatar}
-                                    />
-                                </div>
-                            </div>
-
-                            <div className="content-wrap">
-                                {/* 用户名 */}
-                                <div className="user-info">
-                                    <div className="user-name">{item.user.uname}</div>
-                                </div>
-                                {/* 评论内容 */}
-                                <div className="root-reply">
-                                    <span className="reply-content">{item.content}</span>
-                                    <div className="reply-info">
-                                        {/* 评论时间 */}
-                                        <span className="reply-time">{item.ctime}</span>
-                                        {/* 评论数量 */}
-                                        <span className="reply-time">点赞数:{item.like}</span>
-                                        {/* 删除显示-条件渲染，拿到当前项id，以此id为条件对评论列表做过滤or直接讲将内容清空*/}
-                                        {user.uid === item.user.uid && <span className="delete-btn"
-                                                                             onClick={() => handleDel(item.rpid)}>
-                                    删除
-                                </span>}
-
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    {commentList.map(item => <Item key={item.id} item={item} onDel={handleDel}/>
                     )}
                 </div>
             </div>
