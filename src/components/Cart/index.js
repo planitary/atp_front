c31271c0d8256bd0d56a38af8b2c19a0d009ab1a
planitary <853652156@qq.com>
2024-03-12 22:40:19 +0800

    20240312 add:美团案例-购物车添加与金额计算

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,74 +1,94 @@
 import classNames from 'classnames'
 import Count from '../Count'
 import './index.scss'
+import {useDispatch, useSelector} from "react-redux";
+import {itemMinus,itemPlus} from "../../meituan-store/modules/takeaway";
+import {cartClear} from "../../meituan-store/modules/takeaway";
 
 const Cart = () => {
-  const cart = []
-  return (
-    <div className="cartContainer">
-      {/* 遮罩层 添加visible类名可以显示出来 */}
-      <div
-        className={classNames('cartOverlay')}
-      />
-      <div className="cart">
-        {/* fill 添加fill类名可以切换购物车状态*/}
-        {/* 购物车数量 */}
-        <div className={classNames('icon')}>
-          {true && <div className="cartCornerMark">{0}</div>}
-        </div>
-        {/* 购物车价格 */}
-        <div className="main">
-          <div className="price">
+    const {cartList} = useSelector(state => state.foods)
+
+    const dispatch = useDispatch()
+    // 计算总价
+    const totalPrice = cartList.reduce((a,c) => a + c.price * c.count,0)
+
+    // 便利购物车中的count值，累加商品数量
+    const totalCount = cartList.reduce((a,b) => a + b.count,0)
+    // 写法二
+    // function getTotal(carList){
+    //     let total = 0
+    //     carList.forEach(item => {
+    //         total += item.count
+    //     })
+    //     return total
+    // }
+    return (
+        <div className="cartContainer">
+            {/* 遮罩层 添加visible类名可以显示出来 */}
+            <div
+                className={classNames('cartOverlay')}
+            />
+            <div className="cart">
+                {/* fill 添加fill类名可以切换购物车状态*/}
+                {/* 购物车数量 */}
+                <div className={classNames('icon',totalCount > 0 && 'fill')}>
+                    {totalCount > 0 && <div className="cartCornerMark">{totalCount}</div>}
+                </div>
+                {/* 购物车价格 */}
+                <div className="main">
+                    <div className="price">
             <span className="payableAmount">
               <span className="payableAmountUnit">¥</span>
-              {0.00}
+                {totalPrice}
             </span>
-          </div>
-          <span className="text">预估另需配送费 ¥5</span>
-        </div>
-        {/* 结算 or 起送 */}
-        {false ? (
-          <div className="goToPreview">去结算</div>
-        ) : (
-          <div className="minFee">¥20起送</div>
-        )}
-      </div>
-      {/* 添加visible类名 div会显示出来 */}
-      <div className={classNames('cartPanel')}>
-        <div className="header">
-          <span className="text">购物车</span>
-          <span className="clearCart">
+                    </div>
+                    <span className="text">预估另需配送费 ¥5</span>
+                </div>
+                {/* 结算 or 起送 */}
+                {totalCount > 0  ? (
+                    <div className="goToPreview">去结算</div>
+                ) : (
+                    <div className="minFee">¥20起送</div>
+                )}
+            </div>
+            {/* 添加visible类名 div会显示出来 */}
+            <div className={classNames('cartPanel','visible')}>
+                <div className="header">
+                    <span className="text">购物车</span>
+                    <span className="clearCart" onClick={() => dispatch(cartClear())} >
             清空购物车
           </span>
-        </div>
+                </div>
 
-        {/* 购物车列表 */}
-        <div className="scrollArea">
-          {cart.map(item => {
-            return (
-              <div className="cartItem" key={item.id}>
-                <img className="shopPic" src={item.picture} alt="" />
-                <div className="main">
-                  <div className="skuInfo">
-                    <div className="name">{item.name}</div>
-                  </div>
-                  <div className="payableAmount">
-                    <span className="yuan">¥</span>
-                    <span className="price">{item.price}</span>
-                  </div>
+                {/* 购物车列表 */}
+                <div className="scrollArea">
+                    {cartList.map(item => {
+                        return (
+                            <div className="cartItem" key={item.id}>
+                                <img className="shopPic" src={item.picture} alt=""/>
+                                <div className="main">
+                                    <div className="skuInfo">
+                                        <div className="name">{item.name}</div>
+                                    </div>
+                                    <div className="payableAmount">
+                                        <span className="yuan">¥</span>
+                                        <span className="price">{item.price}</span>
+                                    </div>
+                                </div>
+                                <div className="skuBtnWrapper btnGroup">
+                                    <Count
+                                        count={item.count}
+                                        onPlus={() => dispatch(itemPlus({id: item.id}))}
+                                        onMinus={() => dispatch(itemMinus({id:item.id}))}
+                                    />
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
-                <div className="skuBtnWrapper btnGroup">
-                  <Count
-                    count={item.count}
-                  />
-                </div>
-              </div>
-            )
-          })}
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Cart
--- src/meituan-store/modules/takeaway.js
+++ src/meituan-store/modules/takeaway.js
@@ -9,20 +9,47 @@ const foodsStore = createSlice({
         foodsList: [],
         //  菜单激活active
         activeIndex: 0,
+        // 购物车列表
+        cartList: []
 
     },
-    reducers:{
-        setFoodsList(state,action){
+    reducers: {
+        setFoodsList(state, action) {
             state.foodsList = action.payload
         },
-        changeActiveIndex(state,action){
+        changeActiveIndex(state, action) {
             state.activeIndex = action.payload
+        },
+        addCart(state, action) {
+            // 是否添加过,用action.payload.id去cartList中查找，如果有，则添加过
+            const item = state.cartList.find(item => item.id === action.payload.id)
+            if (item) {
+                item.count++
+            } else {
+                state.cartList.push(action.payload)
+            }
+        },
+        // 购物车增减(关键点:要找到进行操作的对象）
+        itemPlus(state, action) {
+            const item = state.cartList.find(item => item.id === action.payload.id)
+            item.count++
+        },
+        itemMinus(state,action){
+            const item = state.cartList.find(item => item.id === action.payload.id)
+            if (item.count === 0){
+                return
+            }
+            item.count--
+        },
+        // 清空购物车
+        cartClear(state){
+            state.cartList = []
         }
     }
 })
 
 // 异步请求
-const {setFoodsList,changeActiveIndex} = foodsStore.actions
+const {setFoodsList, changeActiveIndex, addCart,itemPlus,itemMinus,cartClear} = foodsStore.actions
 const fetchFoodsList = () => {
     return async (dispatch) => {
         const res = await axios.get("  http://localhost:3004/takeaway")
@@ -31,7 +58,7 @@ const fetchFoodsList = () => {
     }
 }
 
-export {fetchFoodsList,changeActiveIndex}
+export {fetchFoodsList, changeActiveIndex, addCart,itemPlus,itemMinus,cartClear}
 
 const reducer = foodsStore.reducer
 export default reducer
