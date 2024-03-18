1a72c424bf6f8ce8c9fd8fe8ab8bb161e2409e81
planitary <853652156@qq.com>
2024-03-18 21:39:02 +0800

    20240318 add:时间控制器

--- src/billPages/Month/index.js
+++ src/billPages/Month/index.js
@@ -1,7 +1,14 @@
 import {NavBar,DatePicker} from "antd-mobile";
 import './index.scss'
+import {useState} from "react";
 
 const Month = () => {
+  // 控制弹框的打开和关闭
+  const [dateVisible,setDateVisible] = useState(false);
+  const onConfirm = () => {
+    setDateVisible(false)
+  }
+
   return (
       <div className="monthlyBill">
         <NavBar className="nav" backArrow={false}>
@@ -10,11 +17,12 @@ const Month = () => {
         <div className="content">
           <div className="header">
             {/* 时间切换区域 */}
-            <div className="date">
+            <div className="date" onClick={() => setDateVisible(true)}>
             <span className="text">
               2023 | 3月账单
             </span>
-              <span className='arrow expand'></span>
+              {/*弹框状态决定箭头方向 arrow expand朝下 arrow朝上*/}
+              <span className={dateVisible ?'arrow expand' : 'arrow'}/>
             </div>
             {/* 统计区域 */}
             <div className='twoLineOverview'>
@@ -36,7 +44,11 @@ const Month = () => {
                 className="kaDate"
                 title="记账日期"
                 precision="month"
-                visible={false}
+                visible={dateVisible}
+                // 如果设置了onClose,则不需要单独设置onCancel,onConfirm默认会关闭控件，不需要单独指定
+                // onCancel={() => setDateVisible(false)}
+                onConfirm={onConfirm}
+                onClose={() => setDateVisible(false)}
                 max={new Date()}
             />
           </div>
