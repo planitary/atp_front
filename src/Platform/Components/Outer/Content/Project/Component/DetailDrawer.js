2f305bf0c2661bdc11215c5f82ed94bccd516b3a
planitary <853652156@qq.com>
2024-03-28 23:07:05 +0800

    20240328 add:项目编辑-抽屉样式

---
+++ src/Platform/Components/Outer/Content/Project/Component/DetailDrawer.js
@@ -0,0 +1,164 @@
+import React, {useState} from 'react';
+import {PlusOutlined} from '@ant-design/icons';
+import {Button, Col, DatePicker, Drawer, Form, Input, Row, Select, Space} from 'antd';
+
+const {Option} = Select;
+
+
+const DetailDrawer = ({drawerVisible,editData,handleOpen,handleClose}) => {
+    return (
+        <>
+            <Drawer
+                title="编辑项目"
+                width={500}
+                mask={true}
+                onClose={handleClose}
+                open={drawerVisible}
+                styles={{
+                    body: {
+                        paddingBottom: 80,
+                    },
+                }}
+                extra={
+                    <Space>
+                        <Button onClick={handleClose}>取消</Button>
+                        <Button onClick={handleClose} type="primary">
+                            确认
+                        </Button>
+                    </Space>
+                }
+            >
+                <Form layout="vertical" hideRequiredMark>
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <Form.Item
+                                name="projectName"
+                                label="项目名称"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: '项目名称',
+                                    },
+                                ]}
+                            >
+                                <Input placeholder="填写接口名称"/>
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item
+                                name="url"
+                                label="Url"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please enter url',
+                                    },
+                                ]}
+                            >
+                                <Input
+                                    style={{
+                                        width: '100%',
+                                    }}
+                                    addonBefore="http://"
+                                    addonAfter=".com"
+                                    placeholder="Please enter url"
+                                />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <Form.Item
+                                name="owner"
+                                label="Owner"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please select an owner',
+                                    },
+                                ]}
+                            >
+                                <Select placeholder="Please select an owner">
+                                    <Option value="xiao">Xiaoxiao Fu</Option>
+                                    <Option value="mao">Maomao Zhou</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item
+                                name="type"
+                                label="Type"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please choose the type',
+                                    },
+                                ]}
+                            >
+                                <Select placeholder="Please choose the type">
+                                    <Option value="private">Private</Option>
+                                    <Option value="public">Public</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <Form.Item
+                                name="approver"
+                                label="Approver"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please choose the approver',
+                                    },
+                                ]}
+                            >
+                                <Select placeholder="Please choose the approver">
+                                    <Option value="jack">Jack Ma</Option>
+                                    <Option value="tom">Tom Liu</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item
+                                name="dateTime"
+                                label="DateTime"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please choose the dateTime',
+                                    },
+                                ]}
+                            >
+                                <DatePicker.RangePicker
+                                    style={{
+                                        width: '100%',
+                                    }}
+                                    getPopupContainer={(trigger) => trigger.parentElement}
+                                />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col span={24}>
+                            <Form.Item
+                                name="description"
+                                label="Description"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'please enter url description',
+                                    },
+                                ]}
+                            >
+                                <Input.TextArea rows={4} placeholder="please enter url description"/>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form>
+            </Drawer>
+        </>
+    );
+};
+export default DetailDrawer;
--- src/Platform/Components/Outer/Content/Project/ProjectList.js
+++ src/Platform/Components/Outer/Content/Project/ProjectList.js
@@ -1,66 +1,94 @@
 import React, {useEffect, useState} from 'react';
-import {Pagination, Space, Table} from 'antd';
+import {Button, Pagination, Space, Table} from 'antd';
 import qs from 'qs';
 import {useDispatch, useSelector} from "react-redux";
 import {GetPagination, GetProjectList} from "./Store/Modules/ProjectStore";
 import _ from "lodash";
 import dayjs from "dayjs";
 import Page from "../../../Pagination/Page";
-
-const columns = [
-    {
-        title: '项目名',
-        dataIndex: 'projectName',
-        width: '20%',
-    },
-    {
-        title: '项目前缀',
-        dataIndex: 'projectUrl',
-        width: '20%',
-    },
-    {
-        title: '备注',
-        dataIndex: 'remark'
-    },
-    {
-        title: '创建时间',
-        dataIndex: 'createTime',
-        width: '15%'
-    },
-    {
-        title: '更新时间',
-        dataIndex: 'updateTime',
-        width: '15%'
-    },
-    {
-        title: '创建人',
-        dataIndex: 'createUser',
-    },
-    {
-        title: '操作',
-        key: 'operation',
-        render: () => (
-            <Space size="middle">
-                <a>编辑</a>
-                <a style={{"color": "red"}}>删除</a>
-            </Space>
-        )
-    }
-];
-
+import DetailDrawer from "./Component/DetailDrawer";
 
 const ProjectList = () => {
+    const columns = [
+        {
+            title: '项目名',
+            dataIndex: 'projectName',
+            width: '20%',
+        },
+        {
+            title: '项目前缀',
+            dataIndex: 'projectUrl',
+            width: '20%',
+        },
+        {
+            title: '备注',
+            dataIndex: 'remark'
+        },
+        {
+            title: '创建时间',
+            dataIndex: 'createTime',
+            width: '15%'
+        },
+        {
+            title: '更新时间',
+            dataIndex: 'updateTime',
+            width: '15%'
+        },
+        {
+            title: '创建人',
+            dataIndex: 'createUser',
+        },
+        {
+            title: '操作',
+            key: 'operation',
+            render: (_, record) => (
+                <Space size="middle">
+                    <a onClick={() => handleEditClick(record)}>编辑</a>
+                    <a style={{"color": "red"}}>删除</a>
+                </Space>
+            )
+        }
+    ];
+    // 加载状态
     const [loading, setLoading] = useState(false);
     // 分页状态
     // const [pagination,setPagination] = useState({})
+    // 抽屉控制器
+    const [drawerVisible, setDrawerVisible] = useState(false);
+    // 暂存数据值用于抽屉表单的回显
+    const [editData, setEditData] = useState(null)
 
     const dispatch = useDispatch()
+
     // 钩子函数，渲染完页面就访问一次列表页
     useEffect(() => {
-        dispatch(GetProjectList(1,10))
+        dispatch(GetProjectList(1, 10))
 
     }, [dispatch])
 
+    // 编辑按钮回调
+    const handleEditClick = () => {
+        setDrawerVisible(true)
+    }
+
+    // 抽屉关闭事件回调
+    const handleCloseClick = () => {
+        setDrawerVisible(false);
+        setEditData(null);
+    }
+
+    // 分页回调
+    const handlePagination = (currentPage) => {
+        // 更新
+        console.log("currentPage", currentPage)
+        // 根据当前页码，发起新的回调
+        dispatch(GetProjectList(currentPage))
+    }
+
+    // 编辑当前行
+    const currentId = (record) => {
+        return record.id
+    }
 
     // 从回调中拿到数据渲染列表
     const projectListData = useSelector(state => state.projectList)
@@ -71,29 +99,29 @@ const ProjectList = () => {
     // console.log(datas.map((data) => data.id))
     // console.log("resData", resData)
 
-    // 分页回调
-    const handlePagination= (currentPage) => {
-        // 更新
-        console.log("currentPage",currentPage)
-        // 根据当前页码，发起新的回调
-        dispatch(GetProjectList(currentPage))
-    }
-
     return (
+        <>
             <Table
                 columns={columns}
-                rowKey={(record) => record.id}
+                rowKey={currentId}
                 dataSource={rowData}
                 pagination={{
                     pageSize: resData.pageSize,
                     total: resData.totalCounts,
-                    showTotal:(total) => `共 ${total} 条`,
+                    showTotal: (total) => `共 ${total} 条`,
                     onChange: handlePagination
                 }}
                 // pagination={<Page total={23}/>}
                 loading={loading}
                 // onChange={handleTableChange}
             />
+
+            <DetailDrawer
+                editData={editData}
+                drawerVisible={drawerVisible}
+                handleClose={handleCloseClick}
+            />
+        </>
     );
 };
 export default ProjectList;
